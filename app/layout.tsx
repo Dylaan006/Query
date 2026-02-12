68ce2605606b7bd81ddda93fb043dd6e1e73f2f6
Dylan <dylan131206@gmail.com>
2026-02-12 03:02:05 -0300

    Fix favicon 404

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+                <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
             <body className="bg-background-light dark:bg-background-dark font-display text-gray-900 dark:text-gray-100 h-screen overflow-hidden flex selection:bg-primary/30 selection:text-primary-100">
                 {children}
